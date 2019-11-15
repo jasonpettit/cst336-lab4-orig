8feb89f8edea8a9c5482d40c66ec07bfff8848dd
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-15 03:00:47 +0000

    end of step 7

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ app.get("/venus", function(req, res){
     res.render("venus.html");
 });
 
+/*CLOUD 9 CONFIG
 //server listner
 app.listen("8080", "127.0.0.1", function(){
     console.log("Express Server is Running...");
 });
+*/
+
+//HEROKU CONFIG
+//listner
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
+
