14331d48ab39e67d51fb5901c6456c178b49e879
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-15 02:40:06 +0000

    end of step 5

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // JavaScript File
 const express = require("express");
 const app = express();
+app.engine('html',require('ejs').renderFile);
+app.use(express.static("public"));
 
 //routes
 app.get("/", function(req, res){
-    res.send("it works!");
+    res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+    res.render("venus.html");
 });
 
 //server listner
